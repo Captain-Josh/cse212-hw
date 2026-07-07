55f1912d03767443f54d249e89f9758380808cd0
Captain-Josh <jnweke54@gmail.com>
2026-07-07 18:17:39 -0400

    Completed dynamic arrays activity

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -11,9 +11,19 @@ public static class Arrays
         // TODO Problem 1 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+        // be implemented by another person.   
+        // Step 1: Create an array with the required length.
+        double[] result = new double[length];
+        // Step 2: Loop through every position in the array.
+        // Step 3: Calculate the correct multiple for that position.
+        // Step 4: Store the value in the array.
+        // Step 5: Return the completed array
+        for (int i=0; i< length; i++)
+        {
+            result[i] = number*(i+1);
+        }
 
-        return []; // replace this return statement with your own
+        return result; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +39,24 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        
+        // Step 1: Determine where the list should be split, The last amount values will move to the front.
+        // Step 2: Copy the last amount values into a temporary list.
+        // Step 3: Copy the remaining values from the beginning of the list into another temporary list.
+        // Step 4: Clear the original list.
+        // Step 5: Add the values that were at the end of the list first.
+        // Step 6: Add the values that were at the beginning of the list second.
+        // Step 7: The original list is now rotated to the right.
+
+        int splitIndex = data.Count - amount;
+
+        List<int> endPart = data.GetRange(splitIndex, amount);
+        List<int> beginningPart = data.GetRange(0, splitIndex);
+
+        data.Clear();
+
+        data.AddRange(endPart);
+        data.AddRange(beginningPart);
+    
     }
 }
